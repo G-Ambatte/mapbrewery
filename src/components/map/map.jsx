0b4bc9dca941d37a6ab17f8496c24a9811eb18ad
G.Ambatte <sean@robertson-family.nz>
2024-11-12 14:57:25 +1300

    Update color picker params to use local storage

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -11,15 +11,15 @@ import ColorPicker from "../colorPicker/colorPicker";
 
 export default function MapComponent(){
     const [mapURL, setMapURL] = useState('');
-    const [backgroundColor, setBackgroundColor] = useState('#666666');
+    const [bgColor, setBGColor] = useState('');
     const [eventDisplaySettings] = useState({ backgroundPosition: '50% 50%', transform: 'scale(1.0)' });
     
-    return <div className="map" style={{backgroundColor: backgroundColor}}>
+    return <div className="map" style={{backgroundColor: bgColor}}>
         <DisplayComponent imageURL={mapURL} eventDisplaySettings={eventDisplaySettings}></DisplayComponent>
         <MapConfig>
             <h3>Map Configuration</h3>
             <InputComponent storageKey='map' passValue={setMapURL}>Map URL: </InputComponent>
-            <ColorPicker defaultColor={backgroundColor} passValue={setBackgroundColor}>Background Color:</ColorPicker>
+            <ColorPicker defaultColor='#666666' storageKey='BGcolor' passValue={setBGColor}>Background Color:</ColorPicker>
         </MapConfig>
         <EventConfig>
             <h3>Event Configuration</h3>
