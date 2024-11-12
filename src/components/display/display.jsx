a9b2ffb9e7700115030996077bdeade6727c2ea5
G.Ambatte <sean@robertson-family.nz>
2024-11-12 21:18:10 +1300

    Rename "event" to "location"

--- src/components/display/display.jsx
+++ src/components/display/display.jsx
@@ -4,24 +4,29 @@ import { useState } from 'react';
 import { useLocalStorage } from '../../hooks/useStorage';
 
 
-export default function DisplayComponent({imageURL, eventDisplaySettings }){
+export default function DisplayComponent({imageURL, locationDisplaySettings }){
 
-    const eventId = 1;
+    const locationId = 1;
 
     const [mouseTrack, setMouseTrack] = useState(false);
-    const [eventX, setEventX] = useLocalStorage(`event-locX-${eventId}`, 0);
-    const [eventY, setEventY] = useLocalStorage(`event-locY-${eventId}`, 0);
+    const [locationX, setLocationX] = useLocalStorage(`location-locX-${locationId}`, 0);
+    const [locationY, setLocationY] = useLocalStorage(`location-locY-${locationId}`, 0);
    
     function endDrag(e){
         setMouseTrack(false);
         console.log(e);
-        setEventX(e.clientX / e.target.clientWidth);
-        setEventY(e.clientY / e.target.clientHeight);
+        setLocationX(e.clientX / e.target.clientWidth);
+        setLocationY(e.clientY / e.target.clientHeight);
+    }
+
+    function resetLocation(){
+        setLocationX(0.5);
+        setLocationY(0.5);
     }
 
     const locationColor = mouseTrack ? 'white' : 'black';
 
-    return <div className='display' style={{ '--imageSrc': `url(${imageURL}`, ...eventDisplaySettings }} onDragOver={(e)=>{e.preventDefault()}} onDrop={endDrag}>
+    return <div className='display' style={{ '--imageSrc': `url(${imageURL})`, ...locationDisplaySettings }} onDragOver={(e)=>{e.preventDefault()}} onDrop={endDrag}>
         {/* Locations Toolbar */}
         <div className='toolbar'>
         <span
@@ -30,11 +35,11 @@ export default function DisplayComponent({imageURL, eventDisplaySettings }){
             draggable={true}
             onDragStart={()=>{setMouseTrack(true)}}
             onContextMenu={(e)=>{e.preventDefault(); return false;}}
-            onMouseDown={(e)=>{if(e.button != 0){ setEventX(0); setEventY(0); } }}
+            onMouseDown={(e)=>{if(e.button != 0){ resetLocation(); } }}
         >
         </span>
-        <span>X: {(eventX * 100).toFixed(2)}%</span>
-        <span>Y: {(eventY * 100).toFixed(2)}%</span>
+        <span>X: {(locationX * 100).toFixed(2)}%</span>
+        <span>Y: {(locationY * 100).toFixed(2)}%</span>
         <span className='icon interactive fa-solid fa-add' onClick={()=>{alert(1);}}></span>
         </div>
     </div>;
