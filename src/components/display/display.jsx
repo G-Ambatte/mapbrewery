0883ecd82eb2a9520a695d3c51e15fba2d0e5639
G.Ambatte <sean@robertson-family.nz>
2024-11-14 11:10:22 +1300

    Change location to use Ref

--- src/components/display/display.jsx
+++ src/components/display/display.jsx
@@ -1,10 +1,12 @@
 import './display.css'
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useLocalStorage } from '../../hooks/useStorage';
 
 
 export default function DisplayComponent({imageURL, locationDisplaySettings }){
+
+    const displayRef = useRef();
 
     const locationId = 1;
 
@@ -15,8 +17,8 @@ export default function DisplayComponent({imageURL, locationDisplaySettings }){
     function endDrag(e){
         setMouseTrack(false);
         console.log(e);
-        setLocationX(e.clientX / e.target.clientWidth);
-        setLocationY(e.clientY / e.target.clientHeight);
+        setLocationX(e.clientX / displayRef.current.offsetWidth);
+        setLocationY(e.clientY / displayRef.current.offsetHeight);
     }
 
     function resetLocation(){
@@ -26,21 +28,22 @@ export default function DisplayComponent({imageURL, locationDisplaySettings }){
 
     const locationColor = mouseTrack ? 'white' : 'black';
 
-    return <div className='display' style={{ '--imageSrc': `url(${imageURL})`, ...locationDisplaySettings }} onDragOver={(e)=>{e.preventDefault()}} onDrop={endDrag}>
+    return <div ref={displayRef} className='display' style={{ '--imageSrc': `url(${imageURL})`, ...locationDisplaySettings }} onDragOver={(e)=>{e.preventDefault()}} onDrop={endDrag}>
         {/* Locations Toolbar */}
         <div className='toolbar'>
-        <span
-            className='icon interactive fa-solid fa-location-crosshairs'
-            style={{ color: locationColor }}
-            draggable={true}
-            onDragStart={()=>{setMouseTrack(true)}}
-            onContextMenu={(e)=>{e.preventDefault(); return false;}}
-            onMouseDown={(e)=>{if(e.button != 0){ resetLocation(); } }}
-        >
-        </span>
-        <span>X: {(locationX * 100).toFixed(2)}%</span>
-        <span>Y: {(locationY * 100).toFixed(2)}%</span>
-        <span className='icon interactive fa-solid fa-add' onClick={()=>{alert(1);}}></span>
+            <span
+                className='icon interactive fa-solid fa-location-crosshairs'
+                style={{ color: locationColor }}
+                draggable={true}
+                onDragStart={()=>{setMouseTrack(true)}}
+                onContextMenu={(e)=>{e.preventDefault(); return false;}}
+                onMouseDown={(e)=>{if(e.button != 0){ resetLocation(); } }}
+            >
+            </span>
+            <span>X: {(locationX * 100).toFixed(2)}%</span>
+            <span>Y: {(locationY * 100).toFixed(2)}%</span>
+            <span className='icon interactive fa-solid fa-add' onClick={()=>{alert(1);}}></span>
         </div>
+        <div className='location' style={{ '--x': locationX, '--y': locationY }} draggable onDrop={(e)=>{e.preventDefault()}}>1</div>
     </div>;
 };
